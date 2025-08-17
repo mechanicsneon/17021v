c52fd878356be488c0b3989939fdc1d7b4f11550
neonmechanics <team702v@gmail.com>
2025-08-17 10:32:05 -0700

    first commit, added all base vex files/comp template
    added all 6 drivetrain motors, brain, controller, and inertial sensor to global instances.
    
    added controller code to test.
    
    first successful test today 8/17 at 10:28 approx
    
    to fix for next commit: adjust controller deadzones/curves so that the drivetrain is more controllable and less sensitive

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       geoso                                                     */
+/*    Created:      8/17/2025, 10:03:42 AM                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+brain Brain;
+controller Controller1;
+motor leftfront(PORT2, ratio6_1, true);
+motor leftrear(PORT3, ratio6_1, true);
+motor leftrear_stacked(PORT1, ratio6_1, false);
+motor rightfront(PORT9, ratio6_1, true);
+motor rightrear(PORT8, ratio6_1, true);
+motor rightrear_stacked(PORT10, ratio6_1, false);
+inertial inertialsensor = inertial(PORT4);
+motor_group left_drive = motor_group(leftfront, leftrear, leftrear_stacked);
+motor_group right_drive = motor_group(rightfront, rightrear, rightrear_stacked);
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+    left_drive.spin(forward, Controller1.Axis3.position(percent), percent);
+    right_drive.spin(reverse, Controller1.Axis2.position(percent), percent);
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
