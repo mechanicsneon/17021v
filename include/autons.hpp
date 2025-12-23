cd559194d2ecffda726a7eeae158a41a9509fa3f
mechanicsneon <team17021v@gmail.com>
2025-12-23 12:00:14 -0800

    rebasing, added new code for ez template

---
+++ include/autons.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+void default_constants();
+
+void test();
---
+++ include/subsystems.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+inline pros::MotorGroup intake({19, -21, -20}   );
